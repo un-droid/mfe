09086119ef487071c426dd2e9c182cc7ac895a03
un-droid <1alextal@gmail.com>
2023-03-11 13:35:29 +0200

    ci(Container): fixed container webpack.prod public path

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -3,7 +3,7 @@ import { mount } from 'marketing/MarketingApp'
 
 export default () => {
     const ref = useRef(null);
-    console.log("ref.current")
+    console.log("ref.current: ", ref.current)
 
     useEffect(() => {
         mount(ref.current);
