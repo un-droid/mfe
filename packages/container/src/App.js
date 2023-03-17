7fc8189f5b68bf4feea2c4a967a2b51815b468ec
un-droid <1alextal@gmail.com>
2023-03-17 19:56:29 +0200

    feat: added generateClassName and StylesProvider
    to enable randomization of production classes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 
 export default () => {
     return (
     <BrowserRouter>
-        <div>
-            <Header/>
-            <MarketingApp />
-            <h3>hi container!!</h3>
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+            <div>
+                <Header/>
+                <MarketingApp />
+                <h3>hi container!!</h3>
+            </div>
+        </StylesProvider>
     </BrowserRouter>
     );
 };
