9e3a29505abdc87b731bb4c2a1cd6c4572d3f152
un-droid <1alextal@gmail.com>
2023-03-18 16:19:32 +0200

    feat: implemented multi-tier navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,23 @@
 import React, { useRef, useEffect } from 'react';
-import { mount } from 'marketing/MarketingApp'
+import { mount } from 'marketing/MarketingApp';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
-    console.log("ref.current: ", ref.current)
+    const history = useHistory(); //copy of browser history
 
     useEffect(() => {
-        mount(ref.current);
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: ({ pathname: nextPathname}) => {
+                const { pathname } = history.location;
+                // update the history stack, if not the same path to prevent loops
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            }
+        });
+        // parents history listener
+        history.listen(onParentNavigate);
      }, []);
 
     return <div id="marketing-container" ref={ref}></div>
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // mount function to start up the app
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory}) => {
+    const history = defaultHistory || createMemoryHistory();
+    onNavigate && history.listen(onNavigate);
+
     ReactDOM.render (
-        <App></App>,
-        el
-    );
+        <App history={history}></App>, el);
+    
+    return {
+        onParentNavigate({pathname: nextPathname}) {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 };
 
 
@@ -17,7 +28,7 @@ if (process.env.NODE_ENV === 'development') {
     console.log('marketing');
     const devRoot = document.querySelector('#_merketing-dev-root');
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {defaultHistory: createBrowserHistory()});
     }
 }
 
